24226d7c46d5f8c9b40b820c09ca522c1db95dec
dusklight00 <r.rahul.developer@gmail.com>
2024-05-12 14:03:40 +0530

    tailwindcss setup complete

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,9 +1,48 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {
+  Heading,
+  Flex,
+  Spacer,
+  VStack,
+  Button,
+  Center,
+  Box,
+} from "@chakra-ui/react";
+
+import Editor from "@monaco-editor/react";
 
 function App() {
-  return <></>;
+  const code = "var message = 'Monaco Editor!' \nconsole.log(message);";
+
+  return (
+    <div>
+      <Center h="300px">
+        <Flex>
+          <Spacer />
+          <VStack gap="30px">
+            <Heading>AI Code Translator</Heading>
+            <Button colorScheme="blue">Translate</Button>
+          </VStack>
+          <Spacer />
+        </Flex>
+      </Center>
+      <Box border="1px" borderRadius="8px">
+        <Editor
+          height="100px"
+          language="javascript"
+          theme="vs-dark"
+          value={code}
+          borderRadius="8px"
+          options={{
+            inlineSuggest: true,
+            fontSize: "16px",
+            formatOnType: true,
+            autoClosingBrackets: true,
+            minimap: { enabled: false },
+          }}
+        />
+      </Box>
+    </div>
+  );
 }
 
 export default App;
