2942e1c538bfdeec22c2243f7ae29e83d368338f
dusklight00 <r.rahul.developer@gmail.com>
2024-05-12 15:57:08 +0530

    loading feature integrated with translate button

--- view/src/component/TranslateButton.jsx
+++ view/src/component/TranslateButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Button } from "@chakra-ui/react";
 import instance from "../../instance";
@@ -11,14 +11,24 @@ async function translateCode(code, language) {
 }
 
 function TranslateButton({ initialCode, targetLanguage, setTargetCode }) {
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleTranslateCode = async () => {
+    setIsLoading(true);
     const translation = await translateCode(initialCode, targetLanguage);
+    setIsLoading(false);
     setTargetCode(translation);
   };
 
   return (
     <div>
-      <Button colorScheme="blue" size="lg" onClick={handleTranslateCode}>
+      <Button
+        colorScheme="blue"
+        size="lg"
+        onClick={handleTranslateCode}
+        loadingText="Translating..."
+        isLoading={isLoading}
+      >
         Translate
       </Button>
     </div>
